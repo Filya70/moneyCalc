894ceb7268d567543c5f4b4442e2e907085c771c
Filya70 <f0rest70@mail.ru>
2020-04-24 12:41:43 +0300

    started writing js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+const = 
+totalBalance = document.querySelector('.total__balance'),
+totalMoney-income = document.querySelector('.total__money-income'),
+totalMoney-expenses = document.querySelector('.total__money-expenses'),
+historyList = document.querySelector('.history__list'),
+form = document.querySelector('#form'),
+operationName = document.querySelector('.operation__name'),
+operationAmount = document.querySelector('.operation__amount');
